83e1cc1b6db337ff8099d2065de35b8b34441488
adnaneaabbar <adnaneaabbar@gmail.com>
2022-04-06 20:25:45 +0000

    initialize project using spring initializr

---
+++ src/main/java/com/ensiasit/projectx/ProjectxApplication.java
@@ -0,0 +1,13 @@
+package com.ensiasit.projectx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ensiasit/projectx/ProjectxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensiasit.projectx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
