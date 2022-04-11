270bad6c39aae807eda2ae8d3d801580e4037de5
adnaneaabbar <adnaneaabbar@gmail.com>
2022-04-11 17:42:16 +0000

    [27]-auth up and running, tested with postman

--- src/main/java/com/ensiasit/projectx/security/services/UserDetailsImpl.java
+++ src/main/java/com/ensiasit/projectx/security/services/UserDetailsImpl.java
@@ -42,7 +42,7 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     public Long getId() {
