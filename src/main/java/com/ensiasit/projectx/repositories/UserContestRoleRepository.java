ba92d163de81515e5dca28f8f084ce066112eec1
El Mehdi ASSALI <el.mehdi.assali@ibm.com>
2022-04-17 05:48:16 +0200

    Hotfix: push forgotten file

--- src/main/java/com/ensiasit/projectx/repositories/UserContestRoleRepository.java
+++ src/main/java/com/ensiasit/projectx/repositories/UserContestRoleRepository.java
@@ -3,5 +3,8 @@ package com.ensiasit.projectx.repositories;
 import com.ensiasit.projectx.models.UserContestRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserContestRoleRepository extends JpaRepository<UserContestRole, Long> {
+    List<UserContestRole> findAllByUserEmail(String email);
 }
