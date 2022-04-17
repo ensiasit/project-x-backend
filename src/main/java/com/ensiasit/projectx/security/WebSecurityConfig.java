7387811c0ddce9cc05fde6172296da29a207f6fd
El Mehdi ASSALI <assalielmehdi@gmail.com>
2022-04-17 05:36:34 +0200

    [26] Authentication form (#5)

--- src/main/java/com/ensiasit/projectx/security/WebSecurityConfig.java
+++ src/main/java/com/ensiasit/projectx/security/WebSecurityConfig.java
@@ -53,7 +53,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests().antMatchers("/api/v1/auth/**", "/swagger-ui/**").permitAll()
+                .authorizeRequests().antMatchers(
+                        "/api/v1/auth/**",
+                        "/swagger-ui/**",
+                        "/api/v1/contests"
+                ).permitAll()
                 .antMatchers("/api/v1/test/**").permitAll()
                 .anyRequest().authenticated();
 
