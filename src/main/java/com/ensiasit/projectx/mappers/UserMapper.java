ed3b1f31390283532e43eebba6fe1fbfeeba30ef
El Mehdi ASSALI <assalielmehdi@gmail.com>
2022-04-20 19:34:12 +0200

    [19] Users management (#15)

---
+++ src/main/java/com/ensiasit/projectx/mappers/UserMapper.java
@@ -0,0 +1,33 @@
+package com.ensiasit.projectx.mappers;
+
+import com.ensiasit.projectx.dto.UserDto;
+import com.ensiasit.projectx.models.User;
+import com.ensiasit.projectx.services.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserMapper {
+    private final PasswordEncoder encoder;
+    private final AdminService adminService;
+
+    public User fromDto(UserDto userDto) {
+        return User.builder()
+                .email(userDto.getEmail())
+                .username(userDto.getUsername())
+                .password(encoder.encode(userDto.getPassword()))
+                .build();
+    }
+
+    public UserDto toDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .username(user.getUsername())
+                .password("**HIDDEN**")
+                .admin(adminService.isAdmin(user.getEmail()))
+                .build();
+    }
+}
--- src/main/java/com/ensiasit/projectx/services/AuthServiceImpl.java
+++ src/main/java/com/ensiasit/projectx/services/AuthServiceImpl.java
@@ -1,13 +1,11 @@
 package com.ensiasit.projectx.services;
 
-import com.ensiasit.projectx.dto.*;
+import com.ensiasit.projectx.dto.LoginRequest;
+import com.ensiasit.projectx.dto.LoginResponse;
+import com.ensiasit.projectx.dto.UserDto;
 import com.ensiasit.projectx.exceptions.BadRequestException;
-import com.ensiasit.projectx.exceptions.NotFoundException;
-import com.ensiasit.projectx.models.Contest;
+import com.ensiasit.projectx.mappers.UserMapper;
 import com.ensiasit.projectx.models.User;
-import com.ensiasit.projectx.models.UserContestRole;
-import com.ensiasit.projectx.repositories.ContestRepository;
-import com.ensiasit.projectx.repositories.UserContestRoleRepository;
 import com.ensiasit.projectx.repositories.UserRepository;
 import com.ensiasit.projectx.security.jwt.JwtUtils;
 import lombok.RequiredArgsConstructor;
@@ -15,21 +13,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
-    private final PasswordEncoder encoder;
     private final UserRepository userRepository;
-    private final ContestRepository contestRepository;
-    private final UserContestRoleRepository userContestRoleRepository;
-    private final AdminService adminService;
+    private final UserMapper userMapper;
 
     @Override
     public LoginResponse authenticateUser(LoginRequest loginRequest) {
@@ -44,51 +36,15 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
-    public RegisterResponse registerUser(RegisterRequest registerRequest) {
-        if (userRepository.existsByEmail(registerRequest.getEmail())) {
+    public UserDto registerUser(UserDto payload) {
+        if (userRepository.existsByEmail(payload.getEmail())) {
             throw new BadRequestException("Email already taken");
         }
 
-        Optional<Contest> contest = contestRepository.findById(registerRequest.getContestId());
-
-        if (contest.isEmpty()) {
-            throw new BadRequestException("Incorrect contest id");
-        }
-
-        User user = User.builder()
-                .username(registerRequest.getUsername())
-                .email(registerRequest.getEmail())
-                .password(encoder.encode(registerRequest.getPassword()))
-                .build();
+        User user = userMapper.fromDto(payload);
 
         user = userRepository.save(user);
 
-        UserContestRole userContestRole = UserContestRole.builder()
-                .user(user)
-                .contest(contest.get())
-                .role(registerRequest.getRole())
-                .build();
-
-        userContestRoleRepository.save(userContestRole);
-
-        return RegisterResponse.builder()
-                .email(user.getEmail())
-                .build();
-    }
-
-    @Override
-    public UserDto getCurrentUser(String userEmail) {
-        Optional<User> user = userRepository.findByEmail(userEmail);
-
-        if (user.isEmpty()) {
-            throw new NotFoundException("Incorrect user email");
-        }
-
-        return UserDto.builder()
-                .email(user.get().getEmail())
-                .username(user.get().getUsername())
-                .password("HIDDEN")
-                .admin(adminService.isAdmin(userEmail))
-                .build();
+        return userMapper.toDto(user);
     }
 }
