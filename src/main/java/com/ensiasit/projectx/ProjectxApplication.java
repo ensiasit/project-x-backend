d38590ddb1adda257a0c8c1727bd26beba0b7e63
El Mehdi ASSALI <el.mehdi.assali@ibm.com>
2022-04-24 02:52:10 +0200

    Add swagger-ui with authentication token

--- src/main/java/com/ensiasit/projectx/ProjectxApplication.java
+++ src/main/java/com/ensiasit/projectx/ProjectxApplication.java
@@ -2,8 +2,10 @@ package com.ensiasit.projectx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ProjectxApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/ensiasit/projectx/security/WebSecurityConfig.java
+++ src/main/java/com/ensiasit/projectx/security/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -63,5 +64,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/swagger-resources/**",
+                        "/swagger-ui.html",
+                        "/v2/api-docs",
+                        "/webjars/**");
     }
 }
